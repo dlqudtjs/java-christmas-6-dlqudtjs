bdb1ed6974c2354c5b181bebd952b6e9ecd85d37
dlqudtjs <dlqudtjs922@gmail.com>
2023-11-12 00:35:30 +0900

    feat(ExceptionMessage): 주문 개수가 유효하지 않을 때 메시지 추가

--- src/main/java/christmas/constant/ExceptionMessage.java
+++ src/main/java/christmas/constant/ExceptionMessage.java
@@ -3,6 +3,7 @@ package christmas.constant;
 public enum ExceptionMessage {
 
     INVALID_VISIT_DATE("유효하지 않은 날짜입니다. 다시 입력해 주세요."),
+    INVALID_MENU_COUNT("유효하지 않은 주문입니다. 다시 입력해 주세요."),
     INVALID_MENU_NAME("유효하지 않은 주문입니다. 다시 입력해 주세요."),
     ERROR_TAG("[ERROR] ");
 
