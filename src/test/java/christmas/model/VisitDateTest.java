41c6d5959260e1df0d93b4d72b1c23efe1d9487c
dlqudtjs <dlqudtjs922@gmail.com>
2023-11-12 02:59:23 +0900

    test(VisitDateTest): Day 반환 테스트
    docs(README): VisitDate 기능 목록 테스트 체크

--- src/test/java/christmas/model/VisitDateTest.java
+++ src/test/java/christmas/model/VisitDateTest.java
@@ -27,4 +27,15 @@ public class VisitDateTest {
         Assertions.assertThatCode(() -> new VisitDate(visitDate))
                 .doesNotThrowAnyException();
     }
+
+    @DisplayName("Day 반환 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 8, 9,
+            10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+            20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
+            30, 31})
+    void getDay(int visitDate) {
+        VisitDate date = new VisitDate(visitDate);
+        Assertions.assertThat(date.getDay()).isEqualTo(visitDate);
+    }
 }
