859a3dcf9e44e69acb8461779b686655e1903cd8
dlqudtjs <dlqudtjs922@gmail.com>
2023-11-12 01:14:18 +0900

    feat(Order): 주문 유효성 검사 기능 추가
    docs(README): Order 유효성 검사 설명 추가  및 기능 목록 체크

--- src/main/java/christmas/model/Order.java
+++ src/main/java/christmas/model/Order.java
@@ -3,6 +3,8 @@ package christmas.model;
 import christmas.constant.ExceptionMessage;
 import christmas.constant.PlannerConfig;
 import christmas.model.enums.Menu;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Order {
@@ -11,29 +13,61 @@ public class Order {
 
     public Order(Map<String, Integer> tempMenuCountMap) {
         validate(tempMenuCountMap);
+        this.menuCountMap = createMenuCountMap(tempMenuCountMap);
+    }
+
+    private Map<Menu, Integer> createMenuCountMap(Map<String, Integer> tempMenuCountMap) {
+        Map<Menu, Integer> convertedMenuCountMap = new HashMap<>();
+
+        tempMenuCountMap.forEach((menuName, menuCount) -> {
+            Menu menu = Menu.getMenuWithName(menuName);
+            convertedMenuCountMap.put(menu, menuCount);
+        });
+
+        return convertedMenuCountMap;
     }
 
     private void validate(Map<String, Integer> tempMenuCountMap) {
-        tempMenuCountMap.forEach((menuName, menuCount) -> {
-            validateMenuName(menuName);
-            validateMenuCount(menuCount);
+        List<String> menuNames = tempMenuCountMap.keySet().stream().toList();
+        List<Integer> menuCounts = tempMenuCountMap.values().stream().toList();
+
+        validateMenuName(menuNames);
+        validateMenuCount(menuCounts);
+        validateTotalMenuCount(menuCounts);
+    }
+
+    private void validateTotalMenuCount(List<Integer> menuCounts) {
+        int maxMenuCount = PlannerConfig.MAX_TOTAL_MENU_COUNT.getValue();
+
+        if (getTotalMenuCount(menuCounts) > maxMenuCount) {
+            ExceptionMessage message = ExceptionMessage.INVALID_MENU_MAX_COUNT;
+            throw new IllegalArgumentException(message.getMessage());
+        }
+    }
+
+    private void validateMenuName(List<String> menuNames) {
+        menuNames.forEach(menuName -> {
+            if (Menu.getMenuWithName(menuName).equals(Menu.NONE)) {
+                ExceptionMessage message = ExceptionMessage.INVALID_MENU_NAME;
+                throw new IllegalArgumentException(message.getMessage());
+            }
         });
     }
 
-    private void validateMenuName(String menuName) {
-        if (Menu.getMenuWithName(menuName).equals(Menu.NONE)) {
-            ExceptionMessage message = ExceptionMessage.INVALID_MENU_NAME;
-            throw new IllegalArgumentException(message.getMessage());
-        }
-    }
-
-    private void validateMenuCount(int menuCount) {
-        int maxMenuCount = PlannerConfig.MAX_MENU_COUNT.getValue();
+    private void validateMenuCount(List<Integer> menuCounts) {
         int minMenuCount = PlannerConfig.MIN_MENU_COUNT.getValue();
 
-        if (menuCount < minMenuCount || menuCount > maxMenuCount) {
-            ExceptionMessage message = ExceptionMessage.INVALID_MENU_COUNT;
-            throw new IllegalArgumentException(message.getMessage());
-        }
+        menuCounts.forEach(menuCount -> {
+            if (menuCount < minMenuCount) {
+                ExceptionMessage message = ExceptionMessage.INVALID_MENU_COUNT;
+                throw new IllegalArgumentException(message.getMessage());
+            }
+        });
+    }
+
+    private int getTotalMenuCount(List<Integer> menuCounts) {
+        return menuCounts.stream()
+                .mapToInt(Integer::intValue)
+                .sum();
     }
 }
