b0e2f7ba4caf124539584b2be041ec659103477d
dlqudtjs <dlqudtjs922@gmail.com>
2023-11-12 00:33:10 +0900

    feat(PlannerConfig): 플래너에 사용되는 상수 enum 생성

---
+++ src/main/java/christmas/constant/PlannerConfig.java
@@ -0,0 +1,18 @@
+package christmas.constant;
+
+public enum PlannerConfig {
+
+    MAX_MENU_COUNT(20),
+    MIN_MENU_COUNT(1),
+    DECEMBER(12);
+
+    private final int value;
+
+    PlannerConfig(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
