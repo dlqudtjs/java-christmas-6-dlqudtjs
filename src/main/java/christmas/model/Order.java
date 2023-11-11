85603920cc84a4184ac3536c51311f3b11497d0b
dlqudtjs <dlqudtjs922@gmail.com>
2023-11-12 00:36:51 +0900

    feat(Order): 존재하는 메뉴 및 유효한 주문 개수 유효성 검사 기능 추가

--- src/main/java/christmas/model/Order.java
+++ src/main/java/christmas/model/Order.java
@@ -1,5 +1,7 @@
 package christmas.model;
 
+import christmas.constant.ExceptionMessage;
+import christmas.constant.PlannerConfig;
 import christmas.model.enums.Menu;
 import java.util.Map;
 
@@ -7,7 +9,31 @@ public class Order {
 
     private final Map<Menu, Integer> menuCountMap;
 
-    public Order(Map<String, Integer> menuCountMap) {
+    public Order(Map<String, Integer> tempMenuCountMap) {
+        validate(tempMenuCountMap);
+    }
 
+    private void validate(Map<String, Integer> tempMenuCountMap) {
+        tempMenuCountMap.forEach((menuName, menuCount) -> {
+            validateMenuName(menuName);
+            validateMenuCount(menuCount);
+        });
+    }
+
+    private void validateMenuName(String menuName) {
+        if (Menu.getMenuWithName(menuName).equals(Menu.NONE)) {
+            ExceptionMessage message = ExceptionMessage.INVALID_MENU_NAME;
+            throw new IllegalArgumentException(message.getMessage());
+        }
+    }
+
+    private void validateMenuCount(int menuCount) {
+        int maxMenuCount = PlannerConfig.MAX_MENU_COUNT.getValue();
+        int minMenuCount = PlannerConfig.MIN_MENU_COUNT.getValue();
+
+        if (menuCount < minMenuCount || menuCount > maxMenuCount) {
+            ExceptionMessage message = ExceptionMessage.INVALID_MENU_COUNT;
+            throw new IllegalArgumentException(message.getMessage());
+        }
     }
 }
