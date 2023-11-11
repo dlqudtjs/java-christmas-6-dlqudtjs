a3bdfb6824aeefe0f29dda974c2810e4f469aa94
dlqudtjs <dlqudtjs922@gmail.com>
2023-11-12 00:07:49 +0900

    feat(Order): Order 클래스 생성

---
+++ src/main/java/christmas/model/Order.java
@@ -0,0 +1,13 @@
+package christmas.model;
+
+import christmas.model.enums.Menu;
+import java.util.Map;
+
+public class Order {
+
+    private final Map<Menu, Integer> menuCountMap;
+
+    public Order(Map<String, Integer> menuCountMap) {
+
+    }
+}
