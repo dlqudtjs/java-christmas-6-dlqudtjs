7511270239a5f24ba16da627a1181a369a7bdca0
dlqudtjs <dlqudtjs922@gmail.com>
2023-11-12 00:30:52 +0900

    feat(ExceptionMessage): 예외 메시지 enum 생성

---
+++ src/main/java/christmas/constant/ExceptionMessage.java
@@ -0,0 +1,18 @@
+package christmas.constant;
+
+public enum ExceptionMessage {
+
+    INVALID_VISIT_DATE("유효하지 않은 날짜입니다. 다시 입력해 주세요."),
+    INVALID_MENU_NAME("유효하지 않은 주문입니다. 다시 입력해 주세요."),
+    ERROR_TAG("[ERROR] ");
+
+    private final String message;
+
+    ExceptionMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return ERROR_TAG + message;
+    }
+}
