7993e1b2e7c811d761692ba48bafb75755d712f9
dlqudtjs <dlqudtjs922@gmail.com>
2023-11-12 17:32:33 +0900

    feat(Benefit): Giveaway 반환 기능 추가
    docs(README): Benefit 기능 목록 체크

--- src/main/java/christmas/model/Benefit.java
+++ src/main/java/christmas/model/Benefit.java
@@ -1,5 +1,7 @@
 package christmas.model;
 
+import christmas.model.enums.GiveawayType;
+
 public class Benefit {
 
     private final Order order;
@@ -8,5 +10,9 @@ public class Benefit {
     public Benefit(Order order, VisitDate visitDate) {
         this.order = order;
         this.visitDate = visitDate;
+    }
+
+    public Giveaway getGiveaway(GiveawayType giveawayType) {
+        return new Giveaway(giveawayType, order.getTotalPrice());
     }
 }
