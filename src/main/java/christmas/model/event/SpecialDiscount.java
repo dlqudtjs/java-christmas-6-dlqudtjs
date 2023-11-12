7bc0e7a01a4cb93f09b5bddf3de48fa5fd3ef4fb
dlqudtjs <dlqudtjs922@gmail.com>
2023-11-13 01:49:55 +0900

    feat(SpecialDiscount): 특별 날짜 할인 로직을 수행하는 클래스 생성
    docs(README): Benefit 기능 목록 체크

---
+++ src/main/java/christmas/model/event/SpecialDiscount.java
@@ -0,0 +1,27 @@
+package christmas.model.event;
+
+import christmas.model.Amount;
+import christmas.model.BookingInfo;
+
+public class SpecialDiscount implements Event {
+
+    private static final int DISCOUNT_PRICE = 1_000;
+    private final BookingInfo bookingInfo;
+
+    public SpecialDiscount(BookingInfo bookingInfo) {
+        this.bookingInfo = bookingInfo;
+    }
+
+    @Override
+    public Amount getDiscount() {
+        if (isSpecialDay()) {
+            return new Amount(DISCOUNT_PRICE);
+        }
+
+        return new Amount(0);
+    }
+
+    private boolean isSpecialDay() {
+        return bookingInfo.getVisitDate().isSpecialDay();
+    }
+}
