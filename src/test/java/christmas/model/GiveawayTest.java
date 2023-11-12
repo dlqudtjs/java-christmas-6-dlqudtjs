9b052f3f99fa3157d81f373e342e9a22d1818ef1
dlqudtjs <dlqudtjs922@gmail.com>
2023-11-12 17:22:04 +0900

    test(GiveawayTest): 증정품 반환 테스트
    docs(README): Giveaway 기능 목록 테스트 체크

--- src/test/java/christmas/model/GiveawayTest.java
+++ src/test/java/christmas/model/GiveawayTest.java
@@ -1,9 +1,12 @@
 package christmas.model;
 
 import christmas.model.enums.GiveawayType;
+import christmas.model.enums.Menu;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class GiveawayTest {
 
@@ -35,4 +38,36 @@ public class GiveawayTest {
         Assertions.assertThat(giveaway.isExist()).isTrue();
     }
 
+    @DisplayName("기준 금액을 넘었을 때, 샴페인 증정 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {120_000, 130_000, 200_000, 1_000_000})
+    void getChampagne(int price) {
+        // given
+        GiveawayType giveawayType = GiveawayType.DECEMBER_GIVEAWAY;
+        Amount amount = new Amount(price);
+
+        // when
+        Giveaway giveaway = new Giveaway(giveawayType, amount);
+        int size = giveaway.getMenus().size();
+
+        // then
+        Assertions.assertThat(size).isEqualTo(1);
+        Assertions.assertThat(giveaway.getMenus()).containsKey(Menu.CHAMPAGNE);
+        Assertions.assertThat(giveaway.getMenus().get(Menu.CHAMPAGNE)).isEqualTo(1);
+    }
+
+    @DisplayName("기준 금액을 넘지 못했을 때, 샴페인 증정 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 5_000, 100_000, 110_000, 119_000})
+    void getChampagneZero(int price) {
+        // given
+        GiveawayType giveawayType = GiveawayType.DECEMBER_GIVEAWAY;
+        Amount amount = new Amount(price);
+
+        // when
+        Giveaway giveaway = new Giveaway(giveawayType, amount);
+
+        // then
+        Assertions.assertThat(giveaway.getMenus()).containsKey(Menu.NONE);
+    }
 }
