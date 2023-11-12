9ca7918a0cc81425561c10063e2a43c4d1ea04be
dlqudtjs <dlqudtjs922@gmail.com>
2023-11-12 13:40:02 +0900

    feat(Giveaway): 가격에 따른 증정품을 생성하는 기능 추가

---
+++ src/main/java/christmas/model/Giveaway.java
@@ -0,0 +1,35 @@
+package christmas.model;
+
+import christmas.model.enums.GiveawayType;
+import christmas.model.enums.Menu;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Giveaway {
+
+    private final Map<Menu, Integer> menus;
+
+    public Giveaway(GiveawayType giveawayType, Amount amount) {
+        this.menus = getMenu(giveawayType, amount);
+    }
+
+    private Map<Menu, Integer> getMenu(GiveawayType giveawayType, Amount amount) {
+        if (isAvailable(giveawayType, amount)) {
+            return generateMenu(giveawayType, amount);
+        }
+
+        return null;
+    }
+
+    private Map<Menu, Integer> generateMenu(GiveawayType giveawayType, Amount amount) {
+        return giveawayType.getMenus().stream()
+                .collect(Collectors.toMap(
+                        menu -> menu,
+                        menu -> giveawayType.getQuantity(amount.getValue())
+                ));
+    }
+
+    private boolean isAvailable(GiveawayType giveawayType, Amount amount) {
+        return amount.isGreaterThan(new Amount(giveawayType.getPrice()));
+    }
+}
