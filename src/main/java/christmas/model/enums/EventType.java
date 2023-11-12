4238eb98db67006cee6abc101093e7762e3ba60a
dlqudtjs <dlqudtjs922@gmail.com>
2023-11-13 02:08:21 +0900

    feat(EventType): getEventFactory 메서드 Override
    docs(README): EventType 기능 목록 체크

--- src/main/java/christmas/model/enums/EventType.java
+++ src/main/java/christmas/model/enums/EventType.java
@@ -1,14 +1,38 @@
 package christmas.model.enums;
 
+import christmas.model.event.ChristmasDDayDiscount;
+import christmas.model.event.EventFactory;
+import christmas.model.event.Giveaway;
+import christmas.model.event.WeekDiscount;
 import java.util.Arrays;
 import java.util.List;
 
 public enum EventType {
 
-    CHRISTMAS_D_DAY_DISCOUNT("크리스마스 디데이 할인", true),
-    WEEKDAY_DISCOUNT("평일 할인", true),
-    WEEKEND_DISCOUNT("주말 할인", true),
-    GIFT_EVENT("증정 이벤트", true);
+    CHRISTMAS_D_DAY_DISCOUNT("크리스마스 디데이 할인", true) {
+        @Override
+        public EventFactory getEventFactory() {
+            return ChristmasDDayDiscount::new;
+        }
+    },
+    WEEKDAY_DISCOUNT("평일 할인", true) {
+        @Override
+        public EventFactory getEventFactory() {
+            return WeekDiscount::new;
+        }
+    },
+    WEEKEND_DISCOUNT("주말 할인", true) {
+        @Override
+        public EventFactory getEventFactory() {
+            return WeekDiscount::new;
+        }
+    },
+    GIFT_EVENT("증정 이벤트", true) {
+        @Override
+        public EventFactory getEventFactory() {
+            return Giveaway::new;
+        }
+    };
 
     private final String name;
     private final boolean active;
@@ -31,4 +55,6 @@ public enum EventType {
     public boolean isActive() {
         return active;
     }
+
+    public abstract EventFactory getEventFactory();
 }
