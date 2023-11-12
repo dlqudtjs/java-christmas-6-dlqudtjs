58215682c0471456c367aed1abaa9e8fdee41219
dlqudtjs <dlqudtjs922@gmail.com>
2023-11-12 13:38:53 +0900

    feat(GiveawayType): 증정품 enum 생성 및 기능 목록 추가
    docs(README): GiveawayType 기능 목록 추가 및 체크

---
+++ src/main/java/christmas/model/enums/GiveawayType.java
@@ -0,0 +1,35 @@
+package christmas.model.enums;
+
+import java.util.List;
+
+public enum GiveawayType {
+
+    DECEMBER_GIVEAWAY(List.of(Menu.CHAMPAGNE), 120_000, false);
+
+    private final List<Menu> menus;
+    private final int price;
+
+    private final boolean isPerUint;
+
+    GiveawayType(List<Menu> menus, int price, boolean isPerUint) {
+        this.menus = menus;
+        this.price = price;
+        this.isPerUint = isPerUint;
+    }
+
+    public List<Menu> getMenus() {
+        return menus;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getQuantity(int price) {
+        if (isPerUint) {
+            return price / this.price;
+        }
+
+        return 1;
+    }
+}
