ecb4a28bf682af0e652cfc3aa96963fa2827b052
dlqudtjs <dlqudtjs922@gmail.com>
2023-11-13 18:09:18 +0900

    docs(README): Event 관련 설명 추가

--- src/main/java/christmas/model/event/WeekendDiscount.java
+++ src/main/java/christmas/model/event/WeekendDiscount.java
@@ -1,7 +1,7 @@
 package christmas.model.event;
 
-import static christmas.model.event.EventConfig.MEEKEND_EVENT_MENU_TYPE;
 import static christmas.model.event.EventConfig.WEEKEND_DISCOUNT_PRICE;
+import static christmas.model.event.EventConfig.WEEKEND_EVENT_MENU_TYPE;
 
 import christmas.model.Amount;
 import christmas.model.BookingInfo;
@@ -28,7 +28,7 @@ public class WeekendDiscount implements PlannerEvent {
     }
 
     private int getMenuCountByMenuType() {
-        return bookingInfo.getOrder().getMenuTypeCount(MEEKEND_EVENT_MENU_TYPE);
+        return bookingInfo.getOrder().getMenuTypeCount(WEEKEND_EVENT_MENU_TYPE);
     }
 
     private boolean isWeekend() {
