44b159d79755e82a91998ce95930ef649fb3fe8b
dlqudtjs <dlqudtjs922@gmail.com>
2023-11-13 23:09:09 +0900

    test(AmountTest): 기존 금액 할인하는 기능 추가
    docs(README): Amount 테스트 체크

---
+++ src/test/java/christmas/model/AmountTest.java
@@ -0,0 +1,25 @@
+package christmas.model;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class AmountTest {
+
+    @DisplayName("할인 후 금액 반환 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 5_000, 100_000, 110_000, 119_000})
+    void getDiscountedPrice(int price) {
+        // given
+        Amount amount = new Amount(price);
+
+        // when
+        Amount minusPrice = new Amount(5_000);
+        Amount expectedPrice = new Amount(price - 5_000);
+        amount.discount(minusPrice);
+
+        // then
+        Assertions.assertThat(amount.getValue()).isEqualTo(expectedPrice.getValue());
+    }
+}
