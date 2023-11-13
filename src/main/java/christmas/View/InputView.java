9f27d987bac5849911b00da04a3c95e869b9e7ba
dlqudtjs <dlqudtjs922@gmail.com>
2023-11-14 02:19:12 +0900

    feat(InputView): 식당 방문 날짜를 입력받는 View 추가

---
+++ src/main/java/christmas/View/InputView.java
@@ -0,0 +1,18 @@
+package christmas.View;
+
+import camp.nextstep.edu.missionutils.Console;
+import christmas.constant.PlannerMessage;
+import christmas.util.Parser;
+
+public class InputView {
+
+    private InputView() {
+    }
+
+    public static int readVisitDate() {
+        PlannerMessage message = PlannerMessage.INPUT_VISIT_DATE_MESSAGE;
+        System.out.println(message.getMessage());
+
+        return Parser.parseToInt(Console.readLine());
+    }
+}
