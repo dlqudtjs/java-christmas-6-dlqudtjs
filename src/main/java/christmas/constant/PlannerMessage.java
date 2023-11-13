c4ec4917ff8ece2d46b406c61a3ed81c7811564a
dlqudtjs <dlqudtjs922@gmail.com>
2023-11-14 02:12:00 +0900

    feat(PlannerMessage): Planner 메시지 enum 생성

---
+++ src/main/java/christmas/constant/PlannerMessage.java
@@ -0,0 +1,25 @@
+package christmas.constant;
+
+public enum PlannerMessage {
+
+    PLANNER_START_MESSAGE("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다."),
+    INPUT_VISIT_DATE_MESSAGE("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)"),
+    INPUT_MENU_COUNT_MESSAGE("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)"),
+    EVENT_BENEFIT_PREVIEW_MESSAGE("12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!"),
+    OUTPUT_ORDER_MENU_MESSAGE("<주문 메뉴>"),
+    OUTPUT_TOTAL_ORDER_PRICE_BEFORE_DISCOUNT_MESSAGE("<할인 전 총주문 금액>"),
+    OUTPUT_GIVEAWAY_MENU_MESSAGE("<증정 메뉴>"),
+    OUTPUT_BENEFIT_DETAILS_MESSAGE("<혜택 내역>"),
+    OUTPUT_TOTAL_EXPECTED_PAYMENT_AFTER_DISCOUNT_MESSAGE("<할인 후 예상 결제 금액>"),
+    OUTPUT_EVENT_BADGE_MESSAGE("<12월 이벤트 배지>");
+
+    private final String message;
+
+    PlannerMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
