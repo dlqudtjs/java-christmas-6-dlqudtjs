4a2fbd6ff72e8b13aaa2959251de1c9dcd38f02f
dlqudtjs <dlqudtjs922@gmail.com>
2023-11-14 11:54:20 +0900

    feat(PlannerController): 메뉴 입력 로직 추가
    docs(README): PlannerController 기능 목록 체크

--- src/main/java/christmas/controller/PlannerController.java
+++ src/main/java/christmas/controller/PlannerController.java
@@ -2,12 +2,19 @@ package christmas.controller;
 
 import christmas.View.InputView;
 import christmas.View.OutputView;
+import christmas.constant.CommonSymbol;
+import christmas.model.Order;
 import christmas.model.VisitDate;
+import christmas.util.Parser;
+import java.util.List;
+import java.util.Map;
 
 public class PlannerController {
 
     public void run() {
         VisitDate visitDate = createVisitDate();
+        Order order = new Order(createOrder());
+
     }
 
     private VisitDate createVisitDate() {
@@ -18,6 +25,25 @@ public class PlannerController {
                 OutputView.printExceptionMessage(e.getMessage());
             }
         } while (true);
+    }
+
+    private Map<String, Integer> createOrder() {
+        do {
+            try {
+                List<String> menuCounts = parseMenuCount(readMenuCount(), CommonSymbol.COMMA);
+                return Parser.parseToMap(menuCounts, CommonSymbol.DASH);
+            } catch (IllegalArgumentException e) {
+                OutputView.printExceptionMessage(e.getMessage());
+            }
+        } while (true);
+    }
+
+    private String readMenuCount() {
+        return InputView.readMenuCount();
+    }
+
+    private List<String> parseMenuCount(String menuCount, CommonSymbol symbol) {
+        return Parser.parseToList(menuCount, CommonSymbol.COMMA);
     }
 
     private int readVisitDate() {
